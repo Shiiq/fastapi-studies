f594042b9f4b0ace4ba7beda953445afabbb4b92
Shiiq <anjunatown@gmail.com>
2024-01-10 22:42:11 +0300

    Oh no

--- fastapi_studies/infrastructure/config_loader/config_loader.py
+++ fastapi_studies/infrastructure/config_loader/config_loader.py
@@ -1,18 +1,21 @@
 from dataclasses import dataclass
 
 from fastapi_studies.api.config import ApiConfig
+from fastapi_studies.infrastructure.database.config import DBConfig
 from fastapi_studies.infrastructure.redis.config import RedisConfig
 
 
-@dataclass
+@dataclass(frozen=True, slots=True)
 class Config:
 
     api: ApiConfig
+    db: DBConfig
     redis: RedisConfig
 
 
 def load_config() -> Config:
     return Config(
         api=ApiConfig(),
+        db=DBConfig(),
         redis=RedisConfig()
     )
