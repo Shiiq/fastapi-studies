0995b542efaa8f9b82353d65f6bc1b0940d51d6b
Shiiq <anjunatown@gmail.com>
2024-01-22 08:36:55 +0300

    I don't want to do this, but my coworker says it's part of the code standard

--- fastapi_studies/api/routers/movies.py
+++ fastapi_studies/api/routers/movies.py
@@ -4,8 +4,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from redis.asyncio.client import Redis
 
 from fastapi_studies.api.dependencies import get_db_session, get_redis_client
-from fastapi_studies.api.dependencies.setup import DBG
-
+from fastapi_studies.infrastructure.database.repositories import MovieRepo
+from fastapi_studies.api.dependencies.stub import Stub
 
 movies_router = APIRouter(prefix="/movies")
 
@@ -13,8 +13,11 @@ movies_router = APIRouter(prefix="/movies")
 @movies_router.get("/get")
 async def get_movies_by_genre(
         genre: list[str] = Query(default=None),
-        dbgw: DBG = Depends()
+        movie_repo: Stub(MovieRepo) = Depends()
 ):
     print(genre, type(genre))
-    print(f"DB GATEWAY ID {id(dbgw)}, SESSION ID {id(dbgw.s)}")
+    print(
+        f"DB GATEWAY ID {id(movie_repo)}, "
+        f"SESSION ID {id(movie_repo._session)}"
+    )
     return {"hello": "world"}
